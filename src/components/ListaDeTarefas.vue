6ce7ab33a1ed95c3ca410808f9ff0b6e647e9fac
Tomás Fava <tomsfava@gmail.com>
2025-01-07 10:44:03 -0300

    componentizando e add feat v-if

---
+++ src/components/ListaDeTarefas.vue
@@ -0,0 +1,23 @@
+<script setup>
+    const props = defineProps(['tarefas'])
+</script>
+
+<template>
+    <ul class="list-group mt-4" v-if="tarefas.length>0">
+        <li class="list-group-item" v-for="tarefa in tarefas">
+            <input @change="evento=>tarefa.finalizada=evento.target.checked" :checked="tarefa.finalizada" :id="tarefa.título" type="checkbox">
+            <label :class="{done: tarefa.finalizada}" class="ms-3" :for="tarefa.título">
+                {{ tarefa.título }}
+            </label>
+        </li>
+    </ul>
+    <p v-else class="mt-4 ms-1">
+        Não há tarefas pendentes.
+    </p>
+</template>
+
+<style scoped>
+  .done {
+    text-decoration: line-through;
+  }
+</style>
